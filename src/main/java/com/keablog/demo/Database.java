d0b1c5737f697a079e3f2a4c9144142e2133a939
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2019-03-12 11:25:19 +0100

    Har lavet lidt database-arbejde. En message-klasse er blevet lavet, hvor vi bare laver nye instanser i en anden klasse.

--- src/main/java/com/keablog/demo/Database.java
+++ src/main/java/com/keablog/demo/Database.java
@@ -1,15 +1,32 @@
 package com.keablog.demo;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
 
+    private PreparedStatement preparedStatement;
     private Connection connection;
+    private ResultSet resultSet;
+    private String query;
 
     public Database() throws SQLException {
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost/products?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false", "root", "");
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost/blog?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false", "root", "");
     }
+
+    public void createPost(Message message) throws SQLException {
+        query = "INSERT INTO blogposts (`post_title`, `post_text`) VALUES (?, ?)";
+        executeQuery(query, message);
+    }
+
+    public void executeQuery(String query, Message message) throws SQLException {
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(3, message.title);
+        preparedStatement.setString(4, message.text);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
+
+
+
+
 
 }
