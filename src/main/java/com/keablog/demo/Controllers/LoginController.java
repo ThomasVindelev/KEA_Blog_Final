b727bb5e6d6235d7cb3c7e4513965698de613a64
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2019-03-13 14:26:12 +0100

    Forbindelse til gearhost-database er oprettet, og beskeder kan nu sendes til databasen.

--- src/main/java/com/keablog/demo/Controllers/LoginController.java
+++ src/main/java/com/keablog/demo/Controllers/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
 @Controller
