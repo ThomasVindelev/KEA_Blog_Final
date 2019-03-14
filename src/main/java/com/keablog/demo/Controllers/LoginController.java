3f3b955281e7468a38edb07a0cad060c9cc51183
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2019-03-14 15:14:06 +0100

    Tester http-sessions

--- src/main/java/com/keablog/demo/Controllers/LoginController.java
+++ src/main/java/com/keablog/demo/Controllers/LoginController.java
@@ -26,13 +26,13 @@ public class LoginController {
     public String login(HttpSession httpSession, @ModelAttribute User user, Model model, Database database) throws SQLException {
 
         if (loginService.verifyUser(user)) {
+            httpSession.setAttribute("id", user.getId());
             httpSession.setAttribute("username", user.getUsername());
+            httpSession.setAttribute("id_role", user.getId_role());
             return "admin";
         } else {
             model.addAttribute("invalid", true);
             return "login";
         }
-
     }
-
 }
--- src/main/java/com/keablog/demo/Service/MessageService.java
+++ src/main/java/com/keablog/demo/Service/MessageService.java
@@ -48,5 +48,4 @@ public class MessageService {
         }
         return messageList;
     }
-
 }
