df85921f3c6b634550d82a78ea35458eb589829d
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2019-03-18 17:57:22 +0100

    Admins kan nu slette beskeder fra både /blog og /history - Users kan kun læse. Edit-knappen virker ikke endnu.

--- src/main/java/com/keablog/demo/Controllers/MessageController.java
+++ src/main/java/com/keablog/demo/Controllers/MessageController.java
@@ -5,6 +5,7 @@ import com.keablog.demo.Service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -26,8 +27,17 @@ public class MessageController {
 
     @PostMapping("/deletemessage")
     public String deleteMessage(@ModelAttribute (name="Message") Message message) throws SQLException {
-        System.out.println(message.getId());
         messageService.deleteMessage(message);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/editmessage")
+    public String editMessage() {
+        return "edit";
+    }
+
+    @PostMapping("/editmessage")
+    public String editMessage(@ModelAttribute (name="Message") Message message, Model model) {
         return "redirect:/blog";
     }
 
